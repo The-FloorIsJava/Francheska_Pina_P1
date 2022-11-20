3b444f73ff5b41589bbd526fe2f1802b90bec8f3
Francheska Pina <pinaf9@gmail.com>
2022-11-19 21:11:00 -0500

    Added ConnectionFactoryTestSuite class

--- Francheska_Pina_P1/src/test/java/com/revature/Francheska_Pina_P1/Util/ConnectionFactoryTestSuite.java
+++ Francheska_Pina_P1/src/test/java/com/revature/Francheska_Pina_P1/Util/ConnectionFactoryTestSuite.java
@@ -1,4 +1,19 @@
 package com.revature.Francheska_Pina_P1.Util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ConnectionFactoryTestSuite {
+    @Test
+    public void test_getConnection_returnValidConnection_givenProvidedCredentialsAreCorrect(){
+        try(Connection connection = ConnectionFactory.getConnectionFactory().getConnection()) {
+            System.out.println(connection); // see connection is inside memory
+            Assert.assertNotNull(connection);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
