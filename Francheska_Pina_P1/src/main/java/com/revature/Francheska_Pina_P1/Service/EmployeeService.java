bc5f6ea96b342aa2eac170f2741146714ceb54a9
Francheska Pina <pinaf9@gmail.com>
2022-11-20 22:43:37 -0500

    Added the Employee Service

---
+++ Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Service/EmployeeService.java
@@ -0,0 +1,38 @@
+package com.revature.Francheska_Pina_P1.Service;
+
+import com.revature.Francheska_Pina_P1.DAO.EmployeeDAO;
+import com.revature.Francheska_Pina_P1.Model.Employee;
+
+import java.util.List;
+
+public class EmployeeService {
+
+    private Employee sessionEmployee = null;
+
+    private final EmployeeDAO employeeDAO;
+
+    public EmployeeService(EmployeeDAO employeeDAO){
+        this.employeeDAO = employeeDAO;
+    }
+
+    public Employee addEmployee(Employee employee){
+       return employeeDAO.create(employee);
+    }
+
+    public Employee getEmployee(String name){
+        return null;
+    }
+
+    public void removeEmployee(String name){
+
+    }
+
+    public List<Employee> getAllEmployee(){
+        return employeeDAO.findAll();
+    }
+
+    public Employee getSessionEmployee(){
+        return sessionEmployee;
+    }
+
+}
