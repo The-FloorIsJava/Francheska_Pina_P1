d0cdfba327f80e409678b2aba65f513b04877783
Francheska Pina <pinaf9@gmail.com>
2022-11-21 23:48:52 -0500

    Created the InvalidEmployeeInputException

---
+++ Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Util/Exceptions/InvalidEmployeeInputException.java
@@ -0,0 +1,7 @@
+package com.revature.Francheska_Pina_P1.Util.Exceptions;
+
+public class InvalidEmployeeInputException extends RuntimeException{
+    public InvalidEmployeeInputException(String message) {
+        super(message);
+    }
+}
