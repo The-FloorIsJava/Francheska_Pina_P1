9ae3afd73beb7a4922619011e8874d6df692e71c
Francheska Pina <pinaf9@gmail.com>
2022-11-25 14:55:09 -0500

    Fix the connection problem

--- Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Application.java
+++ Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Application.java
@@ -5,6 +5,7 @@ import com.revature.Francheska_Pina_P1.Controller.TicketController;
 import com.revature.Francheska_Pina_P1.DAO.EmployeeDAO;
 import com.revature.Francheska_Pina_P1.DAO.TicketDAO;
 import com.revature.Francheska_Pina_P1.Service.EmployeeService;
+import com.revature.Francheska_Pina_P1.Service.TicketService;
 import io.javalin.Javalin;
 
 public class Application {
@@ -12,10 +13,11 @@ public class Application {
         Javalin app = Javalin.create().start(8081);
 
         EmployeeService employeeService = new EmployeeService(new EmployeeDAO(), new TicketDAO());
+        TicketService ticketService = new TicketService(new TicketDAO(), new EmployeeDAO());
 
-        new EmployeeController(app, employeeService).employeeEndpoint();
-//        new TicketController(app, employeeService).ticketEndPoint();
-        TicketController ticketController = new TicketController(employeeService);
-        ticketController.ticketEndPoint(app);
+        new EmployeeController(app, employeeService, ticketService).employeeEndpoint();
+        new TicketController(app, employeeService, ticketService).ticketEndPoint();
+//        TicketController ticketController = new TicketController(employeeService);
+//        ticketController.ticketEndPoint(app);
     }
 }
