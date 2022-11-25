388f6c4f1820de3f18e4ee913531402a43ebe7db
Francheska Pina <pinaf9@gmail.com>
2022-11-25 15:00:18 -0500

    Changed tickerService constructor and fic the addEmployee to create from the ticketDAO

--- Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Service/TicketService.java
+++ Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Service/TicketService.java
@@ -1,5 +1,6 @@
 package com.revature.Francheska_Pina_P1.Service;
 
+import com.revature.Francheska_Pina_P1.DAO.EmployeeDAO;
 import com.revature.Francheska_Pina_P1.DAO.TicketDAO;
 import com.revature.Francheska_Pina_P1.Model.Employee;
 import com.revature.Francheska_Pina_P1.Model.Ticket;
@@ -9,22 +10,39 @@ import java.util.List;
 
 
 public class TicketService {
-//    private Ticket sessionTicket = null;
-//    private final TicketDAO ticketDAO;
+
+    private final TicketDAO ticketDAO;
+    private final EmployeeDAO employeeDAO;
     List<Ticket> ticketsList;
 
-
-    public TicketService() {
-//        this.ticketDAO = ticketDAO;
-        ticketsList = new ArrayList<>();
+    public TicketService(TicketDAO ticketDAO, EmployeeDAO employeeDAO) {
+        this.ticketDAO = ticketDAO;
+        this.employeeDAO = employeeDAO;
     }
 
-    public void addEmployee(Ticket employee){
-        ticketsList.add(employee);
+
+
+
+//    public TicketService() {
+//        ticketsList = new ArrayList<>();
+//    }
+
+    public Ticket addEmployee(Ticket ticket){
+       return ticketDAO.create(ticket);
     }
+
+//    public void addEmployee(Ticket employee){
+//        ticketsList.add(employee);
+//    }
 
     public List<Ticket> getAllTicket(){
-        return ticketsList;
+//        return ticketsList;
+        return ticketDAO.findAll();
+
+    }
+
+    public Ticket getTicket(String name){
+        return null;
     }
 
 
