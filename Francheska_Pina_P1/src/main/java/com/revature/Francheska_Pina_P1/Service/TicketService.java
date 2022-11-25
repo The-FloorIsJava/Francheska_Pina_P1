94e2b85abfe56a4e3468d18a485e18cad2385059
Francheska Pina <pinaf9@gmail.com>
2022-11-25 00:46:57 -0500

    Replaced addTicket addEmployee to and added getAllTicket method. Also, TicketService has an array list.

--- Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Service/TicketService.java
+++ Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Service/TicketService.java
@@ -1,23 +1,32 @@
 package com.revature.Francheska_Pina_P1.Service;
 
 import com.revature.Francheska_Pina_P1.DAO.TicketDAO;
+import com.revature.Francheska_Pina_P1.Model.Employee;
 import com.revature.Francheska_Pina_P1.Model.Ticket;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class TicketService {
-    private Ticket sessionTicket = null;
-    private final TicketDAO ticketDAO;
-//    List<Ticket>ticketsList;
+//    private Ticket sessionTicket = null;
+//    private final TicketDAO ticketDAO;
+    List<Ticket> ticketsList;
 
 
-    public TicketService(TicketDAO ticketDAO) {
-        this.ticketDAO = ticketDAO;
+    public TicketService() {
+//        this.ticketDAO = ticketDAO;
+        ticketsList = new ArrayList<>();
     }
 
-    public Ticket addTicket(Ticket ticket){
-        return ticketDAO.create(ticket);
+    public void addEmployee(Ticket employee){
+        ticketsList.add(employee);
     }
+
+    public List<Ticket> getAllTicket(){
+        return ticketsList;
+    }
+
+
 }
 
