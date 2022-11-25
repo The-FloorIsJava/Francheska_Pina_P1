7775aa2cdfbf7fe8800d77203bb00b71a3aae6a6
Francheska Pina <pinaf9@gmail.com>
2022-11-25 00:45:25 -0500

    Added getAllTicketHandler and replace the addTicket with addEmployee on the postTicketHandler

--- Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Controller/TicketController.java
+++ Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Controller/TicketController.java
@@ -5,27 +5,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.Francheska_Pina_P1.DAO.TicketDAO;
 import com.revature.Francheska_Pina_P1.Model.Employee;
 import com.revature.Francheska_Pina_P1.Model.Ticket;
+import com.revature.Francheska_Pina_P1.Service.EmployeeService;
 import com.revature.Francheska_Pina_P1.Service.TicketService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.List;
+
 public class TicketController {
     TicketService ticketService;
-    Javalin app;
+    EmployeeService employeeService;
+//    Javalin app;
 
-    public TicketController(Javalin app) {
-        ticketService = new TicketService(new TicketDAO());
-        this.app = app;
+    public TicketController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+        ticketService = new TicketService();
     }
 
-    public void ticketEndPoint(){
+    public void ticketEndPoint(Javalin app){
         app.post("ticket", this::postTicketHandler);
+        app.get("ticket", this::getAllTicketHandler);
+    }
+
+    private void getAllTicketHandler(Context context) {
+        List<Ticket>allTicket = ticketService.getAllTicket();
+        context.json(allTicket);
     }
 
     private void postTicketHandler(Context context) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Ticket ticket = mapper.readValue(context.body(), Ticket.class);
-        ticketService.addTicket(ticket);
+        ticketService.addEmployee(ticket);
         context.json(ticket);
     }
 }
