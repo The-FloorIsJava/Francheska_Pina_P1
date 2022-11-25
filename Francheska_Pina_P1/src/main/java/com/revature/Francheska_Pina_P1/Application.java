60e73d2a59acc93b2e0e2d1491dd1d226cb68679
Francheska Pina <pinaf9@gmail.com>
2022-11-25 00:39:30 -0500

    Edited the application to fix the connection to javalin

--- Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Application.java
+++ Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Application.java
@@ -1,14 +1,21 @@
 package com.revature.Francheska_Pina_P1;
 
 import com.revature.Francheska_Pina_P1.Controller.EmployeeController;
+import com.revature.Francheska_Pina_P1.Controller.TicketController;
+import com.revature.Francheska_Pina_P1.DAO.EmployeeDAO;
+import com.revature.Francheska_Pina_P1.DAO.TicketDAO;
+import com.revature.Francheska_Pina_P1.Service.EmployeeService;
 import io.javalin.Javalin;
 
 public class Application {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8081);
 
-        new EmployeeController(app).employeeEndpoint();
+        EmployeeService employeeService = new EmployeeService(new EmployeeDAO(), new TicketDAO());
 
-
+        new EmployeeController(app, employeeService).employeeEndpoint();
+//        new TicketController(app, employeeService).ticketEndPoint();
+        TicketController ticketController = new TicketController(employeeService);
+        ticketController.ticketEndPoint(app);
     }
 }
