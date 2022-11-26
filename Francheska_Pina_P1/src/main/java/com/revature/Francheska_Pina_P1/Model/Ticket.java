820d20615429deda459f2fcb120ef64d38baa421
Francheska Pina <pinaf9@gmail.com>
2022-11-25 19:22:37 -0500

    Removed the username and password in ticket

--- Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Model/Ticket.java
+++ Francheska_Pina_P1/src/main/java/com/revature/Francheska_Pina_P1/Model/Ticket.java
@@ -1,10 +1,8 @@
 package com.revature.Francheska_Pina_P1.Model;
 
 public class Ticket {
-    private int empId;
     private int reqId;
-    private String username;
-    private String password;
+    private int empId;
     private String type;
     private double amount;
     private String status;
@@ -12,20 +10,16 @@ public class Ticket {
     public Ticket() {
     }
 
-    public Ticket(int reqId, String username, String password, String type, double amount, String status) {
+    public Ticket(int reqId, String type, double amount, String status) {
         this.reqId = reqId;
-        this.username = username;
-        this.password = password;
         this.type = type;
         this.amount = amount;
         this.status = status;
     }
 
-    public Ticket(int empId, int reqId, String username, String password, String type, double amount, String status) {
+    public Ticket(int empId, int reqId, String type, double amount, String status) {
         this.empId = empId;
         this.reqId = reqId;
-        this.username = username;
-        this.password = password;
         this.type = type;
         this.amount = amount;
         this.status = status;
@@ -37,14 +31,7 @@ public class Ticket {
         this.amount = amount;
     }
 
-    public Ticket(int empId, int reqId, String username, String password, String type, double amount) {
-        this.empId = empId;
-        this.reqId = reqId;
-        this.username = username;
-        this.password = password;
-        this.type = type;
-        this.amount = amount;
-    }
+
 
     public int getEmpId() {
         return empId;
@@ -60,22 +47,6 @@ public class Ticket {
 
     public void setReqId(int reqId) {
         this.reqId = reqId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getType() {
@@ -105,10 +76,8 @@ public class Ticket {
     @Override
     public String toString() {
         return "Ticket{" +
+                "reqId=" + reqId +
                 "empId=" + empId +
-                ", reqId=" + reqId +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
                 ", type='" + type + '\'' +
                 ", amount=" + amount +
                 ", status='" + status + '\'' +
